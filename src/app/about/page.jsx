501f555a68a6086a8e81b5536d2cc4f5ad4ba876
Cheick-T <abdallahtayoro@gmail.com>
2025-07-24 23:16:24 +0200

    about page done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,30 @@ import styles from './about.module.css';
 
 const About = () => {
   return (
-  <div>
+  <div  className={styles.container}>
+    <div className={styles.textContainer}>
+      <h1 className={styles.title}>About Us</h1>
+      <p className={styles.description}>
+        We are a team of passionate individuals dedicated to providing the best service possible. Our mission is to innovate and create solutions that make a difference in the lives of our users. We believe in transparency, integrity, and excellence in everything we do.
+      </p>
+      <div className={styles.boxes}>
+        <div className={styles.box}>
+          <h1> 10 K+</h1>
+          <p>Year of experience</p>
+        </div>
+        <div className={styles.box}>
+          <h1> 10 K+</h1>
+          <p>Year of experience</p>
+        </div>
+        <div className={styles.box}>
+          <h1> 10 K+</h1>
+          <p>Year of experience</p>
+        </div>
+
+      </div>
+    </div>
     <div className={styles.imgContainer}>
-      <Image src ="/about.png" alt="About Us" width={500} height={500} />
+      <Image className={styles.img} src ="/about.png" alt="About Us"  fill />
     </div>
   </div>)
 };
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,12 @@
+import styles from './footer.module.css';
+
 const Footer = () =>{
     return (
-        <div>This is my footer</div>
+        <div className={styles.container}>
+            <div className={styles.logo}>Cheick TAYORO</div>
+            <div className={styles.text}> Cheick TAYORO creative thoughts agency All right reserved</div>
+
+        </div>
     )
 }
 
